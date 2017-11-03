44cdfcefe18111563c3a366a3e4d3563bc01169a
KangHyun Lee <rkd7327@naver.com>
2017-11-03 13:52:59 +0900

    df

---
+++ WebContent/js/header.js
@@ -0,0 +1,19 @@
+$(function(){
+	 $('#header-text').bxSlider({
+		    mode: 'vertical',
+		    controls:false,
+		    pager: true,
+		    auto:true,
+		    speed:400,
+		    pause:2500,
+		    minSlides:1
+		  });
+
+	
+	
+	
+	
+	
+	
+	
+});
